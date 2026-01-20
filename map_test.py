bc9153a61095385f1c8d525d7a52a25b404524f3
skytrees1 <piotrporzycki@Mac.home>
2026-01-20 20:25:32 +0100

    Dodanie licznika zycia

--- map_test.py
+++ map_test.py
@@ -41,6 +41,24 @@ level = copy.deepcopy(board)
 # Zmienne do obsługi punktacji
 score = 0
 game_font = p.font.SysFont("arial", 30) 
+game_over_font = p.font.SysFont("arial", 60)
+lives = 3
+
+def reset_positions():
+    global start_time
+    start_time = p.time.get_ticks()
+
+    player.empty()
+    pinky.empty()
+    clyde.empty()
+    ghost_red.empty()
+    ghost_inky.empty()
+
+    player.add(CapMan())
+    pinky.add(Pinky())
+    clyde.add(Clyde())
+    ghost_red.add(red())
+    ghost_inky.add(inky())
 
 def check_point_collision(player_sprite, current_level, current_score):
     # Obliczamy indeksy kafelka, na którym znajduje się środek gracza
@@ -60,6 +78,27 @@ def check_point_collision(player_sprite, current_level, current_score):
             
     return current_score
 
+def show_game_final_score():
+    overlay = p.Surface((WIDTH, HEIGHT))
+    overlay.set_alpha(180) # Przezroczystość (0-255)
+    overlay.fill((0,0,0))
+    screen.blit(overlay, (0,0))
+    
+    text_surf = game_over_font.render("GAME OVER", True, "red")
+    score_surf = game_font.render(f"Wynik końcowy: {score}", True, "white")
+    
+    # Centrowanie napisów
+    text_rect = text_surf.get_rect(center=(WIDTH/2, HEIGHT/2 - 50))
+    score_rect = score_surf.get_rect(center=(WIDTH/2, HEIGHT/2 + 20))
+    
+    screen.blit(text_surf, text_rect)
+    screen.blit(score_surf, score_rect)
+    p.display.flip()
+    
+    # Zamrożenie gry na 3 sekundy
+    p.time.delay(3000)
+
+reset_positions()
 running = True
 
 while running:
@@ -82,6 +121,22 @@ while running:
 
     player.update(player.sprite.direction)
 
+    if (pinky.sprite.collision(player.sprite)or 
+            clyde.sprite.collision(player.sprite)or
+            ghost_red.sprite.collision(player.sprite)or
+            ghost_inky.sprite.collision(player.sprite)):
+
+            lives -= 1
+            print(f"HP DOWN\nREAMANING: {lives}")
+            player.draw(screen)
+            p.display.flip()
+            if lives > 0:
+                p.time.delay(1000)
+                reset_positions()
+            else:
+                show_game_final_score()
+                running = False
+
     screen.fill('black')
     draw_map(screen, level)
     
@@ -96,13 +151,10 @@ while running:
     score_text = game_font.render(f"Licznik punktów: {score}", True, "white")
     screen.blit(score_text, (50, HEIGHT - 40))
 
-    if (pinky.sprite.collision(player.sprite)or 
-        clyde.sprite.collision(player.sprite)or
-        ghost_red.sprite.collision(player.sprite)or
-        ghost_inky.sprite.collision(player.sprite)):
+    lives_text = game_font.render(f"Życia: {lives}", True, "red")
+    screen.blit(lives_text, (WIDTH - 150, HEIGHT - 40))
 
-        print(f"GAME OVER! TWÓJ WYNIK: {score}")
-        running = False
+    
 
     p.display.flip()
     clock.tick(60)
